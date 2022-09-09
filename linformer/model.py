91242221ea23dd7a804f0cd0392cf35d63411338
Yan Sidyakin <sidyakinian@gmail.com>
2022-09-08 20:25:51 -0700

    Add MLP layer

--- linformer/model.py
+++ linformer/model.py
@@ -1,5 +1,22 @@
 import tensorflow as tf
 
+from tensorflow.keras.layers import Dense, LayerNormalization, Dropout
+
+class MLP(tf.keras.layers.Layer):
+    def __init__(self, d_ff, d_output, activation, dropout):
+        super().__init__()
+        self.fc1 = Dense(d_ff, activation=activation)
+        self.fc2 = Dense(d_output)
+        self.dropout = Dropout(dropout)
+
+    def call(self, x):
+        x = self.fc1(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
 class Linformer(tf.keras.Model):
     def __init__(self):
         super().__init__()
@@ -9,4 +26,3 @@ class Linformer(tf.keras.Model):
     def call(self, inputs):
         x = self.dense1(inputs)
         return self.dense2(x)
-
