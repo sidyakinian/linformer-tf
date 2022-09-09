2a4aa45a87444893e4b3008befdb341e79011562
Yan Sidyakin <sidyakinian@gmail.com>
2022-09-08 20:55:36 -0700

    Add rough linear self-attention

--- linformer/model.py
+++ linformer/model.py
@@ -16,11 +16,29 @@ class MLP(tf.keras.layers.Layer):
         x = self.drop(x)
         return x
 
-class MultiHeadSelfAttention(tf.keras.layers.Layer):
-    def __init__(self):
+class LinearSelfAttention(tf.keras.layers.Layer):
+    def __init__(self, k: int, dropout: float):
         super().__init__()
+        self.dropout = Dropout(dropout)
+        self.e = Dense(k)
+        self.f = Dense(k)
 
-    def call(self, x):
+    def call(self, K: tf.Tensor, Q: tf.Tensor, V: tf.Tensor) -> tf.Tensor:
+        # shape of K, Q, V: (batch_size, seq_len, d_k)
+        K = self.e(K)
+        K = tf.transpose(K, perm=[0, 2, 1])
+        QK = tf.matmul(Q, K) / tf.math.sqrt(tf.cast(self.d_k, tf.float32))
+        P_bar = tf.nn.softmax(QK, axis=-1)
+        P_bar = self.dropout(P_bar)
+        V = self.f(V)
+        return tf.matmul(P_bar, V)
+
+class MultiHeadSelfAttention(tf.keras.layers.Layer):
+    def __init__(self, n_heads: int):
+        super().__init__()
+        self.n_heads = n_heads
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
         return x
 
 
