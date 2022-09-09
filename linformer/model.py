d2d23ed01f92afff7944e2454eaa5c300a6d9345
Yan Sidyakin <sidyakinian@gmail.com>
2022-09-08 20:34:13 -0700

    Add function signatures and template for MHA

--- linformer/model.py
+++ linformer/model.py
@@ -1,19 +1,26 @@
 import tensorflow as tf
 
-from tensorflow.keras.layers import Dense, LayerNormalization, Dropout
+from tensorflow.keras.layers import Dense, LayerNormalization, Dropout, Activation
 
 class MLP(tf.keras.layers.Layer):
-    def __init__(self, d_ff, d_output, activation, dropout):
+    def __init__(self, d_ff: int, d_output: int, activation: Activation, dropout: float):
         super().__init__()
         self.fc1 = Dense(d_ff, activation=activation)
         self.fc2 = Dense(d_output)
         self.dropout = Dropout(dropout)
 
-    def call(self, x):
+    def call(self, x: tf.Tensor) -> tf.Tensor:
         x = self.fc1(x)
         x = self.drop(x)
         x = self.fc2(x)
         x = self.drop(x)
+        return x
+
+class MultiHeadSelfAttention(tf.keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
+
+    def call(self, x):
         return x
 
 
@@ -23,6 +30,6 @@ class Linformer(tf.keras.Model):
         self.dense1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
         self.dense2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
 
-    def call(self, inputs):
+    def call(self, input: tf.Tensor) -> tf.Tensor:
         x = self.dense1(inputs)
         return self.dense2(x)
